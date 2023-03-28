91af0afa3d99a98c0f458d6279ce13208e784a3d
LingIgor <lingigor1990@gmail.com>
2023-03-28 14:13:59 +0300

    hw11

--- src/index.js
+++ src/index.js
@@ -12,14 +12,20 @@ formEL.addEventListener('submit', onBtnSubmit)
 function onBtnSubmit (e) {
     e.preventDefault()
     fetchFor.querry = e.target.elements.searchQuery.value
-    fetchFor.axiosReturn().then(data => {console.log(data.data.hits),makeMurkup(data.data.hits)})
+    fetchFor.axiosReturn().then(data => {
+      console.log(data)
+      makeMurkup(data.data.hits)
+      if(data.data.total <= data.data.hits.length ||  data.hits.total === 0) {
+        btnLoadEl.classList.add('is-hidden')
+      }
+    })
 }
 
 
 
 function makeMurkup (data) {
     const murkup = data.map(one => `<div class="photo-card">
-    <img src="${one.webformatURL}" alt="" loading="lazy" width = 300px/>
+    <img src="${one.webformatURL}" alt="" loading="lazy" width = 250px/>
     <div class="info">
       <p class="info-item">
         <b>Likes</b>
@@ -39,3 +45,27 @@ function makeMurkup (data) {
 
   divEl.insertAdjacentHTML('beforeend', murkup)
 }
+
+
+// const handleLoadMoreBtnClick = () => {
+//   unsplashAPI.page += 1;
+
+//   unsplashAPI
+//     .fetchPhotos()
+//     .then(data => {
+//       console.log(data);
+
+//       hideElement(loadMoreBtnEl, data.total_pages, unsplashAPI.page);
+
+//       galleryListEl.insertAdjacentHTML(
+//         'beforeend',
+//         createGalleryCards(data.results)
+//       );
+//     })
+//     .catch(err => {
+//       console.warn(err);
+//     });
+// };
+
+// loadMoreBtnEl.addEventListener('click', handleLoadMoreBtnClick);
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
