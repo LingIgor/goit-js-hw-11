8453e8d1bd372c5cd2bc52052b134549dee3c9a2
LingIgor <lingigor1990@gmail.com>
2023-03-30 14:13:02 +0300

    hw 11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { FetchForMyHW } from './axiosFetch';
 import throttle from 'lodash.throttle';
 
@@ -8,7 +10,7 @@ const formEL = document.querySelector('#search-form');
 const btnSubmitEl = document.querySelector('button');
 const divEl = document.querySelector('.gallery');
 const btnLoadEl = document.querySelector('.load-more');
-const THROTLE_TIME = 300;
+const THROTLE_TIME = 1000;
 
 btnLoadEl.classList.add('is-hidden');
 btnSubmitEl.setAttribute('disabled', true);
@@ -42,7 +44,7 @@ function onBtnSubmit(e) {
         );
         btnLoadEl.classList.add('is-hidden');
         return;
-      }
+      }    
 
       if (fetchFor.page * fetchFor.per_page > data.totalHits) {
         Notiflix.Notify.warning(
@@ -71,27 +73,39 @@ function onBtnLoadClick(e) {
 }
 
 function makeMurkup(data) {
+  // Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+
+  // console.log(totalHits)
+
   const murkup = data.map(
-    ({ webformatURL, tags, likes, views, comments, downloads }) =>
-      `<div class="photo-card">      
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = 300px height=200px/>
+    ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+      `<div class="photo-card"> 
+      <a class="gallery__item" href="${largeImageURL}"/>   
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" width = 350px height=200px/>
    <div class="info">
       <p class="info-item">
-        <b>Likes: ${likes}</b>
+        <b>Likes:</b><span>${likes}</span>
       </p>
       <p class="info-item">
-        <b>Views: ${views}</b>
+        <b>Views:</b><span>${views}</span>
       </p>
       <p class="info-item" >
-        <b> Comments: ${comments}</b>
+        <b> Comments:</b><span>${comments}</span>
       </p>
       <p class="info-item">
-        <b>Downloads: ${downloads}</b>
+        <b>Downloads:</b><span>${downloads}</span>
       </p>
     </div>
   </div>`
   );
 
   divEl.insertAdjacentHTML('beforeend', murkup);
-  // btnLoadEl.classList.remove('is-hidden');
+  btnLoadEl.classList.remove('is-hidden');
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    scrollZoom: false,
+  });
 }
